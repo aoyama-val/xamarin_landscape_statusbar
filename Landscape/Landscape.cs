1624ab30ed630af66546e227312db3bc49cc58fb
Aoyama, Shotaro <aoyama@val.co.jp>
2016-09-15 09:03:47 +0900

    init

---
+++ Landscape/Landscape.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Landscape
+{
+	public class App : Application
+	{
+		public App()
+		{
+			var content = new MyContentPage();
+
+			var page = new NavigationPage(content);
+			page.BarTextColor = Color.Aqua;
+
+			MainPage = page;
+			MainPage.SetValue(NavigationPage.BarBackgroundColorProperty, Color.Lime);
+			MainPage.SetValue(NavigationPage.BarTextColorProperty, Color.Red);
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ Landscape/MyContentPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Landscape
+{
+	public class MyContentPage : ContentPage
+	{
+		public MyContentPage()
+		{
+			this.BackgroundColor = Color.Blue;
+			this.Title = "Landscape";
+			this.Content = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.Center,
+				Children = {
+					new Label {
+						HorizontalTextAlignment = TextAlignment.Center,
+						Text = "Welcome to Xamarin Forms!"
+					}
+				}
+			};
+		}
+
+		protected override void OnSizeAllocated(double _width, double _height)
+		{
+			// If the screen size has (actually) changed then modify the content page size
+			// Only necessary for iOS
+			if (Device.OS == TargetPlatform.iOS && Device.Idiom == TargetIdiom.Phone
+			   && (this.Content.Width != _width || this.Content.Height != _height))
+			{
+				// If the device is a Phone, then proper sizing is needed
+				if (Device.Idiom == TargetIdiom.Phone)
+				{
+					// If Landscape orientation
+					if (_width > _height)
+					{
+						Padding = new Thickness(0, 0, 0, 0);
+					}
+					else {
+						Padding = new Thickness(0, 20, 0, 0);
+					}
+				}
+			}
+			base.OnSizeAllocated(_width, _height);
+		}
+
+	}
+}
